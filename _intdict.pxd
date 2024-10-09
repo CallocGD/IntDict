fb2c211542ef7479af99df5cf168e488471b85f1
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-10-08 20:19:25 -0500

    Add files via upload

---
+++ _intdict.pxd
@@ -0,0 +1,7 @@
+include "intdict/u8.pxd"
+include "intdict/u16.pxd"
+include "intdict/u32.pxd"
+# from intdict.fnv cimport *
+# from intdict.u32 cimport *
+# from intdict.u16 cimport *
+# from intdict.u8 cimport *
---
+++ _intdict.pyx
@@ -0,0 +1,9 @@
+# include "intdict/fnv.pyx"
+include "intdict/u8.pyx"
+include "intdict/u16.pyx"
+include "intdict/u32.pyx"
+
+# from intdict.fnv cimport *
+# from intdict.u32 cimport *
+# from intdict.u16 cimport *
+# from intdict.u8 cimport *
